3d5aad10463701caff80ffb2f97a987cf882bfce
cbsahoo__ <36992486+CBSahoo@users.noreply.github.com>
2024-01-03 07:50:48 +0530

    Create copy-list-with-random-pointer.cpp

---
+++ linked-list/copy-list-with-random-pointer.cpp
@@ -0,0 +1,19 @@
+Node* copyRandomList(Node* head) {
+	map<Node*, Node*> mp;
+
+	Node *temp = head;
+
+	while (temp) {
+		mp[temp] = new Node(temp->val);
+		temp = temp->next;
+	}
+
+	temp = head;
+	while (temp) {
+		mp[temp]->next = mp[temp->next];
+		mp[temp]->random = mp[temp->random];
+		temp = temp->next;
+	}
+
+	return mp[head];
+}

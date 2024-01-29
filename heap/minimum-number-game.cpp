875154690ca89bd2421d127c8fd73599c36a0566
cbsahoo__ <36992486+CBSahoo@users.noreply.github.com>
2024-01-30 04:37:47 +0530

    Create minimum-number-game.cpp

---
+++ heap/minimum-number-game.cpp
@@ -0,0 +1,21 @@
+vector<int> numberGame(vector<int>& nums) {
+    priority_queue<int, vector<int>, greater<int>> pq;
+    vector<int> ans;
+    
+    for(int i = 0; i < nums.size(); i++) {
+        pq.push(nums[i]);
+    }
+    
+    while(!pq.empty()) {
+        int alice = pq.top();
+        pq.pop();
+        
+        int bob = pq.top();
+        pq.pop();
+        
+        ans.push_back(bob);
+        ans.push_back(alice);
+    }
+    
+    return ans;
+}
